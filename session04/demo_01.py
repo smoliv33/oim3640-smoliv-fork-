9c8d670695428b040b03d920bf1cbe954bf21691
Zhi Li <lzblack@users.noreply.github.com>
2023-09-11 12:15:33 -0400

    Create demo_01.py

---
+++ session04/demo_01.py
@@ -0,0 +1,34 @@
+# import math
+
+# print(math.sqrt(5))
+
+
+# import random
+
+# print(random.random())
+
+
+# print('I\'m \"OK\".')
+
+# print('\\\t\\')
+# print(r'\\\t\\')
+
+
+# name = input('Please enter your name: ')
+# password = input('Please enter your password: ')
+
+
+# if name == 'Zhi' or password == 'pswd':
+#     print('Hi, welcome!')
+# else:
+#     print('Get out of here, hacker!')
+
+
+age = input('Please enter your age: ')
+# print(age, type(age))
+age = int(age)
+
+if age >= 21:
+    print('Yes, you can.')
+else:
+    print('Sorry.')
