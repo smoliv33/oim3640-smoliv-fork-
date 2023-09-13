42989951e9d03917ee5f6acb382a1141c42af26f
Zhi Li <lzblack@users.noreply.github.com>
2023-09-13 14:51:58 -0400

    Update ex_04_sol.py

--- session04/ex_04_sol.py
+++ session04/ex_04_sol.py
@@ -3,15 +3,15 @@
 a = input("Please enter number 1: >>> ")
 b = input("Please enter number 2: >>> ")  # No validation yet
 
-a = float(a)
-b = float(b)
+a = int(a)
+b = int(b)
 
 
-print(f'{a} + {b} = {a + b: 8.3f}')
-print(f'{a} - {b} = {a - b: 8.3f}')
-print(f'{a} * {b} = {a * b: 8.3f}')
-print(f'{a} / {b} = {a / b: 8.3f}')
-print(f'{a} ^ {b} = {a **b: 8.3f}')
+print(f'{a} + {b} = {a + b: 5d}')
+print(f'{a} - {b} = {a - b: 5d}')
+print(f'{a} * {b} = {a * b: 5d}')
+print(f'{a} / {b} = {a / b: 5.2f}')
+print(f'{a} ^ {b} = {a ** b: 5d}')
 print()
 
 
@@ -29,8 +29,11 @@ ops = {
     '^': operator.pow,
 }
 
-max_int = max(len(str(int(ops[op](a, b)))) for op in ops)
-print(max_int)
+max_res_len = (
+    max(len(str(int(ops[op](a, b)))) for op in ops) + len(str(a)) + len(str(b)) + 1
+)
+
+# print(max_int)
 
 for op in ops:
-    print(f'{a} {op} {b} = {ops[op](a, b):>{max_int+4}.3f}')
+    print(f'{a} {op} {b} = {ops[op](a, b):{max_res_len}.2f}')
